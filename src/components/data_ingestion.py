425c6ecc1042bbdb8e77c9ab15634d4892c7b048
Reetesh V <reeteshrohi@gmail.com>
2025-04-15 14:10:32 +0530

    created datainjection module

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,50 @@
+# read datasets from different sources
+
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass 
+
+
+@dataclass
+class DataInjectionConfig:
+    train_data_path: str=os.path.join('artifacts', "train.csv")
+    test_data_path: str=os.path.join('artifacts', "test.csv")
+    raw_data_path: str=os.path.join('artifacts', "data.csv")
+
+
+class DataInjection:
+    def __init__(self):
+        self.injection_config = DataInjectionConfig()
+
+    def initiate_data_injection(self):
+        logging.info("entered the data injection method ")
+        try:
+            df = pd.read_csv("notebook\data\stud.csv")
+            logging.info("read the dataset as dataframe")
+
+            os.makedirs(os.path.dirname(self.injection_config.train_data_path), exist_ok=True)
+
+            df.to_csv(self.injection_config.raw_data_path, index=False, header=True) 
+
+            logging.info("train test initiated")
+
+            train_set, test_set =  train_test_split(df, test_size=0.2, random_state=42)
+            train_set.to_csv(self.injection_config.train_data_path, index=False, header=True) 
+
+            test_set.to_csv(self.injection_config.test_data_path, index=False, header=True) 
+
+            logging.info("injection of data is completed")
+
+            return (
+                self.injection_config.train_data_path, self.injection_config.test_data_path
+            )
+        except Exception as e:
+            raise CustomException(e, sys) 
+
+if __name__=="__main__":
+    obj=DataInjection()
+    obj.initiate_data_injection()
